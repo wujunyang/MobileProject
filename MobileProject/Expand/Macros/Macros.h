9f5d373c39777947d877c3fedfec33690174f817
wujunyang <wujunyang@126.com>
2016-01-06 23:19:58 +0800

    增加一些常见的宏定义（Macros.h文件）

--- MobileProject/Expand/Macros/Macros.h
+++ MobileProject/Expand/Macros/Macros.h
@@ -13,9 +13,16 @@
 
 #define UNICODETOUTF16(x) (((((x - 0x10000) >>10) | 0xD800) << 16)  | (((x-0x10000)&3FF) | 0xDC00))
 #define MULITTHREEBYTEUTF16TOUNICODE(x,y) (((((x ^ 0xD800) << 2) | ((y ^ 0xDC00) >> 8)) << 8) | ((y ^ 0xDC00) & 0xFF)) + 0x10000
+
 // App Frame Height&Width
 #define App_Frame_Height        [[UIScreen mainScreen] applicationFrame].size.height
 #define App_Frame_Width         [[UIScreen mainScreen] applicationFrame].size.width
+
+//获取View的属性
+#define GetViewWidth(view)  view.frame.size.width
+#define GetViewHeight(view) view.frame.size.height
+#define GetViewX(view)      view.frame.origin.x
+#define GetViewY(view)      view.frame.origin.y
 
 // MainScreen Height&Width
 #define Main_Screen_Height      [[UIScreen mainScreen] bounds].size.height
@@ -49,8 +56,7 @@
 // 是否空对象
 #define IS_NULL_CLASS(OBJECT) [OBJECT isKindOfClass:[NSNull class]]
 
-/************************************* Color ****************************************************/
-
+//色值
 #define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
 #define RGB(r,g,b) RGBA(r,g,b,1.0f)
 
@@ -59,11 +65,49 @@
 #define COLOR_RGB(rgbValue,a) [UIColor colorWithRed:((float)(((rgbValue) & 0xFF0000) >> 16))/255.0 green:((float)(((rgbValue) & 0xFF00)>>8))/255.0 blue: ((float)((rgbValue) & 0xFF))/255.0 alpha:(a)]
 
 
-
+//App版本号
 #define appVersion [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
 
+//AppDelegate对象
 #define AppDelegateInstance [[UIApplication sharedApplication] delegate]
 
+//获取图片资源
+#define GetImage(imageName) [UIImage imageNamed:[NSString stringWithFormat:@"%@",imageName]]
+
+//在Main线程上运行
+#define DISPATCH_ON_MAIN_THREAD(mainQueueBlock) dispatch_async(dispatch_get_main_queue(), mainQueueBlock);
+
+//主线程上Demo
+//DISPATCH_ON_MAIN_THREAD(^{
+    //更新UI
+//})
+
+//在Global Queue上运行
+#define DISPATCH_ON_GLOBAL_QUEUE_HIGH(globalQueueBlocl) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), globalQueueBlocl);
+#define DISPATCH_ON_GLOBAL_QUEUE_DEFAULT(globalQueueBlocl) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), globalQueueBlocl);
+#define DISPATCH_ON_GLOBAL_QUEUE_LOW(globalQueueBlocl) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), globalQueueBlocl);
+#define DISPATCH_ON_GLOBAL_QUEUE_BACKGROUND(globalQueueBlocl) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), globalQueueBlocl);
+
+//Global Queue
+//DISPATCH_ON_GLOBAL_QUEUE_DEFAULT(^{
+    //异步耗时任务
+//})
+
+//判断是否要Log
+#ifdef NEED_DEBUG
+#define NSLog(format, ...) \
+do { \
+NSLog(@"<%@ : %d : %s>-: %@", \
+[[NSString stringWithUTF8String:__FILE__] lastPathComponent], \
+__LINE__, \
+__FUNCTION__, \
+[NSString stringWithFormat:format, ##__VA_ARGS__]); \
+} while(0)
+#else
+#define NSLog(format, ...) do{ } while(0)
+#endif
+
+//DDLog等级
 #ifdef DEBUG
 static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 #else
